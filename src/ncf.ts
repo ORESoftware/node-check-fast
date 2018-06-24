7d2509060ef77a5396c71fc5920d4cbb940dbcd3
Olegzandr Denman <alex@oresoftware.com>
2018-06-23 17:37:04 -0700

    set:s

--- src/ncf.ts
+++ src/ncf.ts
@@ -7,6 +7,7 @@ import assert = require('assert');
 import os = require('os');
 import path = require('path');
 import util = require('util');
+import readline = require('readline');
 import colors from 'chalk';
 
 const flattenDeep = function (a: Array<any>): Array<any> {
@@ -32,112 +33,130 @@ export interface CalledBackData {
   file: string
 }
 
-declare type NCFCallback = (err: Error | String, data: Array<CalledBackData>) => void;
+declare type NCFCallback = (err?: Error | String, data?: Array<CalledBackData>) => void;
 
 //////////////////////////////////////////////////////////////////////
 
 export const ncf = function (opts: NCFOpts, cb: NCFCallback) {
-  
+
   const root = opts.root || process.cwd();
   assert(path.isAbsolute(root), ' => node-check-fast => Root must be an absolute path.');
-  
+
   const paths = opts.paths || ['*.js'];
   assert(Array.isArray(paths), '  => node-check-fast => "path" must be an array.');
-  
+
   const notPaths = opts.notPaths || ['**/node_modules/**'];
   assert(Array.isArray(notPaths), ' => node-check-fast => "notPaths" must be an array.');
-  
+
   const maxDepth = opts.maxDepth || 12;
   assert(Number.isInteger(maxDepth), '  => node-check-fast => "maxDepth" must be an integer.');
-  
+
   const concurrency = opts.concurrency || cpuCount;
   assert(Number.isInteger(concurrency), ' => "concurrency" option must be an integer.');
   log.info('using concurrency:', concurrency);
-  
+
   ////////////////////////////////////////////////////////////////////////////////
-  
-  const checkAll = function (files: Array<string>, cb: NCFCallback) {
-    
-    async.mapLimit(files, concurrency, function (f, cb) {
-      
+
+
+  const results = [] as Array<any>;
+
+  const processFile = function (f: string) {
+
+    return function (cb: any) {
+
       const k = cp.spawn('bash');
       k.stdin.end(`node -c "${f}";\n`);
-      
+
       k.once('exit', function (code) {
-        
+
         if (code < 1 && opts.verbosity > 1) {
           log.info('The following file was processed with no syntax errors:', colors.blueBright(f));
         }
-        
+
         let err = null;
-        
+
         if (code > 0) {
           err = new Error('Exit code of "node -c" child process was greater than 0 for file => "' + f + '"');
         }
-        
-        cb(err, {code: code, file: f});
-        
+
+        results.push({code: code, file: f});
+
+        cb(err);
+
       });
-      
-    }, cb);
-    
+
+    }
   };
-  
+
+
+  const q = async.queue(function (task: any, cb) {
+    task(cb);
+  }, concurrency);
+
+  const final = function (err?: any) {
+    q.kill();
+    cb(err, results);
+  };
+
+  q.drain = final;
+  q.error = final;
+
+
   const $base = ['find', `${root}`].join(' ');
   const $maxD = ['-maxdepth', `${maxDepth}`].join(' ');
   const $typeF = ['-type f'];
-  
+
   const $path = paths.map(function (p: String) {
     return ' -path \"' + String(p).trim() + '\" ';
   });
-  
+
   const $notPath = notPaths.map(function (p: String) {
     return ' -not -path \"' + String(p).trim() + '\" ';
   });
-  
+
   if (opts.verbosity > 2) {
     log.info('verbose => "--path" option contents => ', util.inspect($path));
     log.info('verbose => "--not-path" option contents => ', util.inspect($notPath));
   }
-  
+
   const cmd = flattenDeep([$base, $maxD, $typeF, $path, $notPath]).join(' ');
   const k = cp.spawn('bash');
-  
-  k.stdin.write('\n' + cmd + '\n');
-  
-  process.nextTick(function () {
-    k.stdin.end();
-  });
-  
+
+  k.stdin.end('\n' + cmd + '\n');
+
   k.stdout.setEncoding('utf8');
   k.stderr.setEncoding('utf8');
-  
-  let stdout = '', stderr = '';
-  
-  k.stdout.on('data', function (data: string) {
-    stdout += data;
+
+  let stderr = '';
+
+  const rl = readline.createInterface({
+    input: k.stdout,
   });
-  
+
+  rl.on('line', function (f: string) {
+    q.push(processFile(f));
+  });
+
   k.stderr.on('data', function (d: string) {
     stderr += d;
   });
-  
+
   k.once('exit', function (code: Number) {
-    
+
+    const idle = q.idle();
+    const running = q.running();
+
     if (code > 0) {
-      return cb(new Error('Error: find command failed - \n' + cmd + '\n' + stderr), []);
+      return final(new Error('Error: find command failed - \n' + cmd + '\n' + stderr));
     }
-    
-    const files = String(stdout).trim().split('\n').filter(l => l);
-    
-    if (files.length < 1) {
-      return cb(null, []);
+
+    if (idle && running < 1) {
+      // queue will be idle if nothing was ever pushed to it
+      cb(null, results);
     }
-    
-    checkAll(files, cb);
-    
+
   });
-  
+
 };
 
 export default ncf;
